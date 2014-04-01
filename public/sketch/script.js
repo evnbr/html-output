59a57e9aebd0849f4db080b1122a609f789e2305
Evan Brooks <brooks.ev@gmail.com>
2014-04-01 09:20:19 -0400

    fixes for presentation

--- public/sketch/script.js
+++ public/sketch/script.js
@@ -5,28 +5,28 @@ var counter = 0;
 
 var mouse = {x: 0, y:0};
 
-function draw() {
+// function draw() {
   
-  counter++;
+  // counter++;
   
-  background(176, 218, 274);  
+  // background(176, 218, 274);  
   
-  for (var i = 0; i < 9; i++) {  
-    var add = Math.sin(counter / 23) * i * 27; 
-    rect(
-      180 + i * 20,
-      86 +  i * 21,
-      52 + add,
-      87
-    );       
-  }
+  // for (var i = 0; i < 9; i++) {  
+  //  var add = Math.sin(counter / 23) * i * 27; 
+  //  rect(
+  //    180 + i * 20,
+  //    86 +  i * 21,
+  //    52 + add,
+  //    87
+  //  );       
+  //}
   
   // console.log(Math.random());
 
-}
+// }
 
 document.addEventListener("mousemove", function(e) {
   // console.log(e.clientX);
-  mouse.x = e.clientX;
-  mouse.y = e.clientY;
+  // mouse.x = e.clientX;
+  // mouse.y = e.clientY;
 });
