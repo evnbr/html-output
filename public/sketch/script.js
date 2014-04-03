9dfdab0ce687f2217816899d82ab8c27c5b181b8
Evan Brooks <brooks.ev@gmail.com>
2014-04-02 20:57:24 -0400

    edits

--- public/sketch/script.js
+++ public/sketch/script.js
@@ -1,32 +1,35 @@
 
-
-
 var counter = 0;
 
 var mouse = {x: 0, y:0};
 
-// function draw() {
+function draaw() {
   
-  // counter++;
+  counter++;
   
-  // background(176, 218, 274);  
+  background(176, 220, 176);  
   
-  // for (var i = 0; i < 9; i++) {  
-  //  var add = Math.sin(counter / 23) * i * 27; 
-  //  rect(
-  //    180 + i * 20,
-  //    86 +  i * 21,
-  //    52 + add,
-  //    87
-  //  );       
-  //}
+  
+  for (var i = 0; i < 14; i++) {  
+    var add = Math.sin(counter / 57) * i * 15;  
+    rect(
+      144 + i * 20, 
+      86 +  i * 21, 
+      100 + add, 
+      87
+    );       
+  }
   
   // console.log(Math.random());
-
-// }
+} 
 
 document.addEventListener("mousemove", function(e) {
-  // console.log(e.clientX);
+  //console.log(e.clientX);
   // mouse.x = e.clientX;
   // mouse.y = e.clientY;
 });
+
+document.addEventListener("mousedown", function(e) {
+  //console.log("click");
+}); 
+
